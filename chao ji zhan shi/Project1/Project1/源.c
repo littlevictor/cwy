5dab5c9056e747c5ea866cfe6492682dde12ffeb
cwy <1540445399@qq.com>
2021-11-02 18:35:58 +0800

    xin de jie jue fang an

--- chao ji zhan shi/Project1/Project1/源.c
+++ chao ji zhan shi/Project1/Project1/源.c
@@ -1,35 +1,42 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
+#include<conio.h>
 
 int num, x, y,W, Wa, Wb, Wc, Wd,shuru;
 char area,area1;
 
 int zuobiao(int, int);
 int init();
-void jiuren(char);
+void jiuren(char*);
 
 int main() {
+	//这是对rand函数进行初始化，同时生成超级战士和守卫转盘的部分{
 	num = init();
 	W = num % 100;
 	Wa = num % 10 + 80;
 	Wb = num % 10 + 40;
 	Wc = num % 10 + 60;
 	Wd = num % 10 + 10;
-
+	//}
+	//这个部分是由用户对超级战士的坐标进行输入{
 	printf("请输入你现在的x坐标");
 	scanf_s("%d", &x);
 	printf("\n\n请输入你现在的y坐标");
 	scanf_s("%d", &y);
-
+	//}
+	//这是对输入的数据进行判断超级战士所处的区域{
 	area = zuobiao(x,y);
-
-	if (area != 'O') {
-		scanf_s("%d", &shuru);
+	//}
+	//如果不需要进行轮盘转动环节则跳过，如果要转，则请用户在键盘上按键
+	if (area != 'O'&&area!='E'&&area!='F') {
+		printf("请在键盘上随机键入一个按键转动轮盘\n");
+		shuru = _getch();
 	}
-
+	//}
+	//这是基于轮盘输出最后的结果{
 	jiuren(area);
-
+	//}
 	return 0;
 }
 
